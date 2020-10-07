b0b57ad2f112224a5148655cbafd5913ee44c328
Rohit1220 <65952260+Rohit1220@users.noreply.github.com>
2020-10-07 18:45:20 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,62 @@
+var player,computer;
+var player_img,computer_img;
+var explosion;
+var score = 0
+var coins,coinsIMG;
+var play = 1;
+var end = 0;
+var gameState = play;
+var left_arrow,right_arrow;
+function preload()
+{
+  player_img = loadImage("fighterplane.png");
+  computer_img = loadImage("computerplane.png"); 
+  explosion = loadImage("explosion.jpg");  
+  coinsIMG = loadAnimation("coin.png","coin1.png","coin2.png","coin3.png","coin4.png");
+}
+function setup(){
+  canvas = createCanvas(500,1000);
+  player = createSprite(100,900);
+  computer = createSprite(400,900);
+  left_arrow = createSprite(50,950);
+  right_arrow = createSprite(450,950);
+  coins = createSprite(random(0,500),random(0,900))
+}
+function draw(){
+  background("white");
+  if (gameState === play){
+  coins.addAnimation("coins",coinsIMG);
+  player.addImage(player_img);
+  computer.addImage(computer_img);
+  computer.velocityY = -2;
+  player.velocityY = -2.5;
+  if (keyDown (LEFT_ARROW))
+  {
+    player.velocityX = -5;
+  }
+  if(keyWentUp (LEFT_ARROW))
+  {
+    player.velocityX = 0;
+  }
+  if (keyDown (RIGHT_ARROW))
+  {
+    player.velocityX = 5;
+  }
+  if (keyWentUp (RIGHT_ARROW))
+  {
+    player.velocityX = 0;
+  }
+  if (player.isTouching(computer))
+  {
+    gameState = end
+    }
+  }
+  if(gameState === end){
+    player.addImage(explosion);
+    player.scale = 0.1
+    player.velocityX = 0;
+    player.velocityY = 0;
+    computer.velocityY = 0;
+  }
+  drawSprites();
+}
